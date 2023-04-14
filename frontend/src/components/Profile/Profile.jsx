a83d51a0ef8dced2be5fdc1ec5963d96d2a8ec52
dev-ashokmanjhu <dev.ashokmanjhu@gmail.com>
2023-04-14 16:16:19 +0530

    remove warnings

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -35,7 +35,7 @@ const Profile = () => {
       <div className="card  min-w-sm border border-gray-700 bg-gray-700 text-gray-50   transition-shadow shadow-xl hover:shadow-xl min-w-max">
         <div className="flex items-center p-4">
           <div className="relative flex flex-col items-center w-full">
-            <div className="h-24 w-24 md rounded-full relative avatar flex items-end justify-end text-purple-400 min-w-max absolute -top-16 flex bg-purple-200 text-purple-100 row-start-1 row-end-3 text-purple-650 ring-1 ring-white">
+            <div className="h-24 w-24 md rounded-full relative avatar items-end justify-end text-purple-400 min-w-max -top-16 flex bg-purple-200 row-start-1 row-end-3 text-purple-650 ring-1 ring-white">
               <img
                 className="h-24 w-24 md rounded-full relative"
                 src="https://pixlok.com/wp-content/uploads/2022/02/Profile-Icon-SVG-09856789.png"
@@ -55,13 +55,13 @@ const Profile = () => {
               </p>
               <div className="py-2 flex space-x-2">
                 <Link to="/newtask">
-                  <button className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max border bg-transparent border-purple-400 text-purple-400 hover:border-purple-800 hover:border-purple-500 px-4 py-1 flex items-center hover:shadow-lg">
+                  <button className="justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max border bg-transparent border-purple-400 text-purple-400 hover:border-purple-500 px-4 py-1 flex items-center hover:shadow-lg">
                     <span className="mr-2"></span>Create Task
                     <span className="ml-2"></span>
                   </button>
                 </Link>
                 <Link to="/updateprofile">
-                  <button className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max text-gray-100 bg-green-500 hover:bg-green-600 px-4 py-1 flex items-center hover:shadow-lg">
+                  <button className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max text-gray-100 bg-green-500 hover:bg-green-600 px-4 py-1 items-center hover:shadow-lg">
                     <span className="mr-2"></span>
                     Edit <span className="ml-2"></span>
                   </button>
@@ -69,14 +69,14 @@ const Profile = () => {
               </div>
               <div className="py-2 flex space-x-2">
                 <button
-                  className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max border bg-transparent border-red-400 text-red-400 hover:border-red-800 hover:border-red-500 px-4 py-1 flex items-center hover:shadow-lg"
+                  className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max border bg-transparent border-red-400 text-red-400 hover:border-red-500 px-4 py-1 items-center hover:shadow-lg"
                   onClick={logoutAllHandler}
                 >
                   <span className="mr-2"></span>Log Out All
                   <span className="ml-2"></span>
                 </button>
                 <button
-                  className="flex justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max text-gray-100 bg-red-500 hover:bg-red-600 px-4 py-1 flex items-center hover:shadow-lg"
+                  className="justify-center  max-h-max whitespace-nowrap focus:outline-none  focus:ring  focus:border-blue-300 rounded max-w-max text-gray-100 bg-red-500 hover:bg-red-600 px-4 py-1 flex items-center hover:shadow-lg"
                   onClick={deleteProfileHandler}
                 >
                   <span className="mr-2"></span>
